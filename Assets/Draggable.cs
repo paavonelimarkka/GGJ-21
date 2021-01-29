88b26164513df3f59d3e6ba090f1e0db401b0003
Taneli Riihimäki <taneli.riihimaki@liveto.io>
2021-01-29 20:33:15 +0200

    Add draggables and container prefab.

---
+++ Assets/Draggable.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Draggable : MonoBehaviour
+{
+    private bool isDragging;
+
+    [SerializeField]
+    private GameObject container;
+
+    private GameController gameController;
+    [SerializeField]
+    public string objectType;
+
+    public void OnMouseDown() {
+        Debug.Log("Plölölöl");
+        isDragging = true;
+    }
+
+    public void OnMouseUp() {
+        isDragging = false;
+    }
+    
+    void OnTriggerStay2D(Collider2D col)
+    {
+        if (col.tag == "Container" && !isDragging) {
+            gameController.setStatusText("Valitsit: " + objectType);
+            Destroy(GetComponent<GameObject>());
+        }
+    }
+
+    void Awake() {
+        gameController = GameObject.Find("/GameController").GetComponent<GameController>();
+    }
+    void Update()
+    {
+        if (isDragging) {
+            Vector2 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
+            transform.Translate(mousePosition);
+        }
+    }
+}
---
+++ Assets/GameController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameController : MonoBehaviour
+{
+    // UI Stuff
+    [SerializeField]
+    private Text statusText;
+
+    // Draggable gameobjects
+    [SerializeField]
+    private List<GameObject> draggables;
+
+    // Container for draggable objects
+    [SerializeField]
+    private GameObject container;
+
+    void Start()
+    {
+        setStatusText("Status");
+    }
+    public void setStatusText(string text) {
+        statusText.GetComponent<Text>().text = text;
+    }
+    void Update()
+    {
+
+    }
+}
