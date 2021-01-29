8d8e8a05b2584670f006ec5c83eb62c9f6592cab
Paavo <paavo.nelimarkka@gmail.com>
2021-01-30 00:50:58 +0200

    add random animal script based on time

---
+++ Assets/Scripts/AnimalRandomizer.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimalRandomizer : MonoBehaviour
+{
+
+    public enum Animals {Bulldog, Lion, Shark};
+    public float randTimer = 1.0f;
+    private int animalsLength;
+    private IEnumerator coroutine;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animalsLength = Animals.GetValues(typeof(Animals)).Length;
+        coroutine = AnimalRandTimer(randTimer);
+        StartCoroutine(coroutine);
+    }
+
+    IEnumerator AnimalRandTimer(float Wait)
+    {
+        while(true) {
+            yield return new WaitForSeconds(Wait);
+            
+            Debug.Log(Wait + " seconds went by");
+            Animals chosenAnimal = (Animals)Random.Range(0,animalsLength);
+            Debug.Log(chosenAnimal);
+        }
+    }
+
+}
