a6cde3d2409351842cdbb5cb6a3c959a8a9db579
Paavo <paavo.nelimarkka@gmail.com>
2021-01-31 00:04:10 +0200

    asdfasdf

--- Assets/Scripts/RatInteract.cs
+++ Assets/Scripts/RatInteract.cs
@@ -6,11 +6,12 @@ public class RatInteract : MonoBehaviour
 {
 
     public string activeItem;
+    public GameObject itemChestOpen;
 
     void OnTriggerStay2D(Collider2D col)
     {
         if (col.tag == "ItemContainer" && Input.GetKey("e")) {
-            Debug.Log("Item container stuff activated");
+            itemChestOpen.SetActive(true);
         }
         if (col.tag == "Queue" && Input.GetKey("e")) {
             Debug.Log("Queue stuff activated");
